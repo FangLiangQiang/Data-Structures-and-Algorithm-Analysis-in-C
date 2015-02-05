f68ef2e921a225578b77f1d66645bdbbd964c493
Fang LiangQiang <fang770489306@sina.cn>
2015-02-05 13:03:15 +0800

    j等于0改位等于1

--- DataStruandAlgAnalysis/DataStruandAlgAnalysis/Algorithm.c
+++ DataStruandAlgAnalysis/DataStruandAlgAnalysis/Algorithm.c
@@ -5,7 +5,7 @@ int MaxSubsequenceSum(const int A[],int N)
 	int ThisSum,MaxSum=0,i,j,k;
 	for(i=0;i<=N;i++)
 	{
-		for (j=0;j<=N;j++)
+		for (j=i;j<=N;j++)
 		{
 			ThisSum = 0;
 			for (k=0;k<=j;k++)
